f0b38ac89170f1c02a58330b781f0fd47c03f29d
BrCOS <cosbrnta@gmail.com>
2025-07-11 19:47:46 -0300

    docs: add code comments

--- src/utils/userInput.py
+++ src/utils/userInput.py
@@ -3,16 +3,19 @@ from pathlib import  Path
 def userInput():
     while True:
         
-        zipped_file = str(input('Paste the path to the zipped file: ')).strip().strip('"')
+        zipped_file = str(input('Paste the path to the zipped file: ')).strip().strip('"')#allow copy as path
 
+        #check if input is empty
         if not zipped_file:
             print('Please enter a valid file path.')
             continue
 
+        #check if Path exists
         if not Path(zipped_file).exists():
             print('The path does not exist or the file was not found.')
             continue
 
+        #check if the Path is a valid zip file
         if not Path(zipped_file).is_file() or Path(zipped_file).suffix.lower() != '.zip':
             print('Only .zip files are allowed!')
             continue
